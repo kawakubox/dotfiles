c0cf4ba85c30bad6c4dcf2fb7bde98006a0e2ca8
kawakubox <shigefumi.kawakubo@gmail.com>
2016-01-13 02:49:02 +0900

    Create setup script.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+unlink ~/.bashrc
+ln -s `pwd $0`/_bashrc ~/.bashrc
+
+unlink ~/.bash_profile
+ln -s `pwd $0`/_bash_profile ~/.bash_profile
+
+unlink ~/.gitconfig
+ln -s `pwd $0`/_gitconfig ~/.gitconfig
+
+unlink ~/.zshrc
+ln -s `pwd $0`/_zshrc ~/.zshrc
