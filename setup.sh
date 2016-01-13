ddf658e4651c13a03b18814e46260f4a37e9cc5e
kawakubox <shigefumi.kawakubo@gmail.com>
2016-01-14 01:40:55 +0900

    Create _gemrc.

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ ln -s `pwd $0`/_bashrc ~/.bashrc
 unlink ~/.bash_profile
 ln -s `pwd $0`/_bash_profile ~/.bash_profile
 
+unlink ~/.gemrc
+ln -s `pwd $0`/_gemrc ~/.gemrc
+
 unlink ~/.gitconfig
 ln -s `pwd $0`/_gitconfig ~/.gitconfig
 
